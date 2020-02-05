5ca9c3ba8be6050ef010a17ea0be6d034efea40c
NemanjaMilenkovic <nemanja.milenkovic@gmail.com>
2020-02-05 18:12:22 +0900

    initial commit

---
+++ seeds/lolcations.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const db = require("../server/knex.js");
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex("table_name")
+    .del()
+    .then(async () => {
+      try {
+        const locations = JSON.parse(fs.readFileSync("../data/locations.json"));
+        for (const location of locations) {
+          const id = location.Site.SiteId;
+          const latitude = location.Site.Latitude;
+          const longitude = location.Site.Longitude;
+          const name = location.Site.SiteName;
+
+          const result = await db("locations").insert({
+            id,
+            latitude,
+            longitude,
+            name,
+          });
+          console.log(result);
+        }
+      } catch (err) {
+        console.error("Error inserting records", err);
+      }
+    })();
+};
