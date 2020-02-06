8d1e6c06efee0bf690a06a7bbb19e38eea90c932
Mary <mary@codechrysalis.io>
2020-02-06 15:34:16 +0900

    Added hacky functionality for Amenities and Truck Services. All contained in Amenities table

--- migrations/20200206110629_amenities.js
+++ migrations/20200206110629_amenities.js
@@ -1,18 +1,16 @@
 exports.up = function(knex) {
   return knex.schema.createTable("amenities", (table) => {
-    table.increments().index();
-    table.integer("location_id");
     table
-      .foreign("location_id")
-      .references("id")
-      .inTable("locations");
-
-    table.text("atm");
+      .increments()
+      .index()
+      .primary();
+    table.text("location_id");
     table.text("wifi");
+    table.text("atm");
+    table.text("truck_tire_care");
     table.text("oil_change");
     table.text("light_mechanical");
     table.text("tire_pass");
-    table.text("truck_tire_care");
   });
 };
 
--- seeds/amenities_seed.js
+++ seeds/amenities_seed.js
@@ -1,9 +1,55 @@
 const allData = require("../data/locations");
 
+//In CustomField
+
+//sortOrder: 1 = Showers
+//sortOrder: 2 = CAT Scales
+//sortOrder: 3 = WiFi +
+//sortOrder: 4 = ATM +
+//sortOrder: 10 = Tire Care +
+//sortOrder: 11 = Oil Change +
+//sortOrder: 12 = Light mech +
+//sortOrder: 13 = Tire Pass +
+
+//async and promise.all
+
 const coreData = allData.map((store) => {
   const insertInfo = {};
+
+  insertInfo.location_id = store.Site.SiteId;
+
+  insertInfo.wifi = store.CustomFields.find(
+    (field) => field.CustomField.SortOrder === 3
+  )
+    ? true
+    : false;
+
+  insertInfo.atm = store.CustomFields.find(
+    (field) => field.CustomField.SortOrder === 4
+  )
+    ? true
+    : false;
+
+  insertInfo.truck_tire_care = store.CustomFields.find(
+    (field) => field.CustomField.SortOrder === 10
+  )
+    ? true
+    : false;
+
   insertInfo.oil_change = store.CustomFields.find(
     (field) => field.CustomField.SortOrder === 11
+  )
+    ? true
+    : false;
+
+  insertInfo.light_mechanical = store.CustomFields.find(
+    (field) => field.CustomField.SortOrder === 12
+  )
+    ? true
+    : false;
+
+  insertInfo.tire_pass = store.CustomFields.find(
+    (field) => field.CustomField.SortOrder === 13
   )
     ? true
     : false;
