a6f52484b978f1efd53368789da67f4b43eb1cdf
Mary <mary@codechrysalis.io>
2020-02-07 10:48:04 +0900

    Results displaying but not filtered ba

--- src/components/RenderRes.jsx
+++ src/components/RenderRes.jsx
@@ -9,11 +9,19 @@ export default function RenderResults(props) {
         <li>{props.locations.name}</li>
         <li>{props.locations.state}</li>
         <li>{props.locations.city}</li>
-        <li>{props.locations.store_id}</li>
-        <li>{props.locations.tel}</li>
-        <li>{props.locations.type}</li>
         <li>{props.locations.zip_code}</li>
-        <div></div>
+        <li>
+          <strong>Store Id: </strong>
+          {props.locations.store_id}
+        </li>
+        <li>
+          <strong>Tel:</strong> {props.locations.tel}
+        </li>
+        <li>{props.locations.type}</li>
+
+        <div>
+          <br></br>
+        </div>
       </div>
     </div>
   );
